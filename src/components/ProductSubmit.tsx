255b0e2a60c1b0a4ffcf14803fbb8028aad26a4e
lvogel0326 <only3boys4us@msn.com>
2024-03-12 10:09:19 -0400

    product submit

--- src/components/ProductSubmit.tsx
+++ src/components/ProductSubmit.tsx
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
+import { Product } from "../Models/Product";
+import { postProductAPI } from "../Services/ProductAPIService";
 export function ProductSubmit() {
+
+  const [nameInput, setNameInput] = useState<string>("");
+  function nameInputHandler(event: SyntheticEvent) {
+    let textBox = event.target as HTMLTextAreaElement;
+    setNameInput(textBox.value);
+  }
+
+  const [priceInput, setPriceInput] = useState<string>("");
+  function priceInputHandler(event: SyntheticEvent) {
+    let textBox = event.target as HTMLTextAreaElement;
+    setPriceInput(textBox.value);
+  }
+
+  const [sellerInput, setSellerInput] = useState<string>("");
+  function sellerInputHandler(event: SyntheticEvent) {
+    let textBox = event.target as HTMLTextAreaElement;
+    setSellerInput(textBox.value);
+  }
+
+  function buttonClickHandler() {
+    let product: Product = {
+      id: 0,
+      name: nameInput,
+      price: parseInt(priceInput),
+      seller: parseInt(sellerInput)
+    };
+    postProductAPI(product);
+  }
+
   return (
     <>
-      <h1>Product Submit placeholder</h1>
+      <h1>Submit a new product</h1>
+      <input
+        onChange = {nameInputHandler}
+        value = {nameInput}
+        placeholder="Enter product name"
+        ></input>
+      
+      <input
+        onChange = {priceInputHandler}
+        value = {priceInput}
+        placeholder="Enter product price"
+        ></input>
+
+      <input
+        onChange = {sellerInputHandler}
+        value = {sellerInput}
+        placeholder="Enter Seller ID"
+        ></input>
+
+      <button onClick = {buttonClickHandler} >Submit</button>
+
     </>
   );
 }
